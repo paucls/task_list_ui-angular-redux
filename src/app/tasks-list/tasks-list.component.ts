92bdc76dba47a78f7f1aba8ddc33db5cf8f49e79
Paulo Clavijo Esteban <paucls@gmail.com>
2017-07-06 22:43:21 +0100

    redux-fork - Integrating with Redux Using NgRedux. WIP

--- src/app/tasks-list/tasks-list.component.ts
+++ src/app/tasks-list/tasks-list.component.ts
@@ -1,6 +1,9 @@
 import { Component, OnInit } from '@angular/core';
 import { TasksService } from './tasks.service';
+import { IAppState } from '../store/i-app-state';
 import { Task } from './task';
+import { NgRedux, select } from '@angular-redux/store';
+import { Observable } from 'rxjs/Observable';
 
 @Component({
   selector: 'app-tasks-list',
@@ -10,32 +13,30 @@ import { Task } from './task';
 })
 export class TasksListComponent implements OnInit {
 
-  tasks: Task[] = [];
+  @select('tasks') tasks$: Observable<Task>;
 
-  constructor(private tasksService: TasksService) {
+  constructor(private ngRedux: NgRedux<IAppState>) {
   }
 
   ngOnInit() {
-    this.tasksService
-      .getAll()
-      .then(tasks => this.tasks = tasks);
+
   }
 
-  addTask(name: string): Promise<number> {
-    name = name.trim();
-    if (!name) {
-      return;
-    }
-
-    let newTask: Task = {name: name};
-
-    return this.tasksService
-      .save(newTask)
-      .then(task => this.tasks.push(task));
-  }
-
-  deleteTask(deletedTask: Task) {
-    this.tasks = this.tasks.filter(task => task.id !== deletedTask.id);
-  }
+  // addTask(name: string): Promise<number> {
+  //   name = name.trim();
+  //   if (!name) {
+  //     return;
+  //   }
+  //
+  //   let newTask: Task = {name: name};
+  //
+  //   return this.tasksService
+  //     .save(newTask)
+  //     .then(task => this.tasks.push(task));
+  // }
+  //
+  // deleteTask(deletedTask: Task) {
+  //   this.tasks = this.tasks.filter(task => task.id !== deletedTask.id);
+  // }
 
 }
