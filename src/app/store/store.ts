c8bc589ac191206503c26cc1de7483f1fd328650
Paulo Clavijo Esteban <paucls@gmail.com>
2017-07-06 22:28:10 +0100

    redux-fork - Configuring the Redux Developer Tools.

--- src/app/store/store.ts
+++ src/app/store/store.ts
@@ -1,5 +1,15 @@
-import { createStore } from 'redux';
+import { environment } from '../../environments/environment';
+import { createStore, compose, GenericStoreEnhancer } from 'redux';
 import { IAppState } from './i-app-state';
 import { reducer } from './reducer';
 
-export const store = createStore<IAppState>(reducer);
+const devToolsExtension: GenericStoreEnhancer = window['devToolsExtension'] ?
+  window['devToolsExtension']() : (f) => f;
+
+let enhancer: GenericStoreEnhancer;
+
+if (!environment.production) {
+  enhancer = compose(devToolsExtension) as GenericStoreEnhancer;
+}
+
+export const store = createStore<IAppState>(reducer, enhancer);
